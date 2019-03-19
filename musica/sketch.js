73247cbca5e3b498bd5de5ca6981512e6f14b886
GRB10 <47344497+GRB10@users.noreply.github.com>
2019-03-18 23:18:02 -0500

    Update sketch.js

--- musica/sketch.js
+++ musica/sketch.js
@@ -1,23 +1,28 @@
+//variables para sonido
 var piano;
 var violin;
 var drums;
 var guitarra;
 
+//variables para imagenes
 var pianoFoto;
 var violinFoto;
 var drumsFoto;
 var guitarraFoto;
 
+// variable del volumen
 var vol;
 
 
 function preload(){
+  
+  //sonidos cargados
   piano= loadSound('assets/piano.mp3');
   drums= loadSound('assets/drums.mp3');
   violin= loadSound('assets/violin.mp3');
   guitarra= loadSound('assets/guitarra.mp3');
   
-  
+  //imagenes cargadas
   pianoFoto= loadImage('assets/piano.png');
   drumsFoto= loadImage('assets/drums.png');
   guitarraFoto= loadImage('assets/guitarra.png');
@@ -30,6 +35,7 @@ function preload(){
 function setup() {
   createCanvas(800, 400);
   
+  //fondos de colores
   noStroke();
   fill(0);
   rect(0,0,200,400);
@@ -41,24 +47,25 @@ function setup() {
   rect(600,0,200,400);
   
   
-  
-    piano.setVolume(0);
+  //volumen inicial
+  piano.setVolume(0);
   violin.setVolume(0);
   drums.setVolume(0);
   guitarra.setVolume(0);
   
-  
+  //repetición de los sonidos
   piano.loop();
   violin.loop();
   drums.loop();
   guitarra.loop();
   
+  //imagenes de los instrumentos
    image(violinFoto, 230, 200, 140, 140);
   image(pianoFoto, 30, 200, 140, 140);
   image(drumsFoto, 630, 200, 140, 140);
   image(guitarraFoto, 430, 200, 140, 140);
   
-  
+  //texto con el título de los instrumentos
   fill(255);
   text("Piano", 80, 380);
   textAlign(CENTER);
@@ -74,10 +81,11 @@ function setup() {
 
 function draw() {
   
-  
+ // control del volumen según la posición en y
    vol = map(mouseY,windowHeight, 0, 0.0, 1.1);
 }
 
+//control del volumen de cada instrumento por medio de un click dentro de su respectivo cuadro
   function mousePressed(){
     if ((mouseX>=0)&&(mouseX<=200)){
     piano.setVolume(vol);
